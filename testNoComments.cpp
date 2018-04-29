d67515119101f80a747ba2cd7298843fc6c73443
taylor-c-rauch <38735721+taylor-c-rauch@users.noreply.github.com>
2018-04-29 14:13:10 -0400

    delete comments from testNoComments.cpp

--- testNoComments.cpp
+++ testNoComments.cpp
@@ -8,17 +8,17 @@ using namespace std;
 
 class Article{
 public:
-  Article();//constructor
-  ~Article();//destructor
-  char letter;//letter assigned to article
-  void print();//print function
-  int numDepOn; //number of articles it is dependent on
-  vector<Article*> dependencies; //list of articles dependent on it
+  Article();
+  ~Article();
+  char letter;
+  void print();
+  int numDepOn; 
+  vector<Article*> dependencies; 
 };
 
 Article::Article(){
-  letter ='.'; //set letter character to a period, a value that will never actually be used to stand in for an article name
-  numDepOn =-1;//set numDepOn to -1, so that no manipulation will cause it to have a value of 0, so will not be printed out
+  letter ='.';
+  numDepOn =-1;
 }
 Article::~Article(){}
 void Article::print(){
@@ -27,19 +27,18 @@ void Article::print(){
 
 class Graph{
 public:
-  Graph();//constructor
-  ~Graph();//destructor
-  Article* findZeroDep();//find article with no dependencies
-  Article* findLeastDep();//find article with fewest dependencies, to take care of cicular dependencies
-  void decDep(Article* b);//decrement numDepOn for a given Article
-  void deleteArticle(Article* a);//remove article from consideration for being printed out
-  int numArt; // number of articles in the graph that need to be "published"
-  Article* graph[26];//stores articles (and dummy articles), used like a hash table, f(letter) = ascii code of letter character - 65
+  Graph();
+  ~Graph();
+  Article* findZeroDep();
+  Article* findLeastDep();
+  void decDep(Article* b);
+  void deleteArticle(Article* a);
+  int numArt;
+  Article* graph[26];
 };
 
 Graph::Graph(){
-  numArt=0; //set total number of articles to 0
-  //initialize graph vector with dummy default articles
+  numArt=0; 
   for (int i=0;i<26;i++){ 
     Article* a = new Article();
     graph[i]=a;
@@ -48,21 +47,18 @@ Graph::Graph(){
 Graph::~Graph(){}
 
 Article* Graph::findZeroDep(){
-  //iterate through all of the articles in the graph
   for(int i=0;i<26;i++){
     Article* a = this->graph[i];
-    //return the first article pointer that depends on no other articles
     if(a->numDepOn==0){
       return a;
     }
-  }//if there are none that have no dependencies, there is a cycle. Find the item with the fewest dependencies
+  }
   return this->findLeastDep();
 }
 
 Article* Graph::findLeastDep(){
   Article* result = this->graph[0];
   int val = result->numDepOn;
-  //iterate through graph to find article with fewest dependencies
   for(int i=1;i<26;i++){
     Article* a = this->graph[i];
     if((a->numDepOn < val)&&(a->numDepOn>0)){
@@ -74,9 +70,7 @@ Article* Graph::findLeastDep(){
 }
 
 void Graph::decDep(Article* b){
-  //calculate the ascii value, subtract 65 to get location in array
   int ascii = ((int)(b->letter))-65;
-  //if we have found the correct article, decrement the number of dependencies it has on other articles
   if(this->graph[ascii]->letter==(b->letter)){
     this->graph[ascii]->numDepOn -=1;
     return;
@@ -84,10 +78,7 @@ void Graph::decDep(Article* b){
 }
 
 void Graph::deleteArticle(Article* a){
-  //this method does not actually delete it, just sets the number of dependencies to -1, so that is will not be in consideration to be printed out again, or found using findZeroDep()
-  //calculate the ascii value, subtract 65 to get location in array
   int ascii = ((int)(a->letter))-65;
-  //if we have found the correct article, set numDepOn to -1
   if(this->graph[ascii]->letter==(a->letter)){
     this->graph[ascii]->numDepOn=-1;
     return;
@@ -95,38 +86,27 @@ void Graph::deleteArticle(Article* a){
 }
 
 vector<char> topologicalSort(Graph g){
-  //create a vector of results
   vector<char> result;
-  //only need to print out the number of articles that were actually stored in the graph, do not consider dummy default articles
   for(int i=0; i<g.numArt;i++){
-    //find the first article that does not depend on any other articles
     Article* a = g.findZeroDep();
-    //add this article's letter identifier to the vector of results
     result.push_back(a->letter);
-    //for every article that depends on this article, deprement its number of dependencies
     for(Article* b: a->dependencies){
       g.decDep(b);
     }
-    //remove the article we just processed from consideration for being found by findZeroDep() again
     g.deleteArticle(a);
   }
   return result;
 }
 
 void process(vector<string> vec){
-  //create a graph object
   Graph info;
   char s1;
   char s2;
-  //iterate through the given vector
   for(int i=0; i<vec.size();i++){
-    //identify the letters used for articles
     s1 = vec[i][0];
     s2 = vec[i][3];
-    //convert to ascii values, we only want capital letters
     int ascii1 = ((int)(s1))-65;
     int ascii2 = ((int)(s2))-65;
-    //if they are lowercase letters, subtract 35 to store in same place as uppercase letters
     if (ascii1>25){
       ascii1 -=32;
       s1 = (char)ascii1;
@@ -134,7 +114,6 @@ void process(vector<string> vec){
       ascii2 -= 32;
       s2 = (char)ascii2;
     }
-    //check to see if in graph, if not, then add them
     if(info.graph[ascii1]->letter!=(s1)){
       delete info.graph[ascii1];
       Article* x = new Article();
@@ -153,14 +132,11 @@ void process(vector<string> vec){
       info.graph[ascii2]=x;
       info.numArt+=1;
     }
-    //update dependencies
     Article* a1 = info.graph[ascii1];
     Article* a2 = info.graph[ascii2];
     (a1->dependencies).push_back(a2);
   }
-  //do the topological sort
   vector<char> output = topologicalSort(info);
-  //print output vector
   for(int i=0;i<output.size();i++){
     cout<<output[i]<<" ";
   }cout<<endl;
@@ -172,7 +148,6 @@ void process(vector<string> vec){
 
 
 int main(){
-  //vector strings should be of the form "Letter1->Letter2"
   vector<string> test1;
   test1.push_back("A->C");
   test1.push_back("B->D");
